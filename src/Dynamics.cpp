ee93f9e6c7c4faad3425e6096f13b5d85dffba3d
MarvinElz <MarvinElz@gmx.de>
2017-03-08 12:31:39 +0100

    minor

--- src/Dynamics.cpp
+++ src/Dynamics.cpp
@@ -214,8 +214,10 @@ bool propagate( std_srvs::SetBool::Request& req, std_srvs::SetBool::Response& re
 
 	ros::Time now = ros::Time::now();
   double dt = (now - last_Prop).toSec();
+
+	//dt = 0.005;
 //	ROS_INFO("dt: %f", dt);
-	if(dt > 0.02) dt = 0.02; 
+	if(dt > 0.005) dt = 0.005; 
 
 	// konstante Schrittweite für Testzwecke
 	//dt = 0.005;								
